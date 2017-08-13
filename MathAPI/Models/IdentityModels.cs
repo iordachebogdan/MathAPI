ea94b28d28d55d51a8b736f2a73b9abda6452aa2
Bogdan Iordache <iordache_bogdan1998@yahoo.com>
2017-08-13 18:04:43 +0300

    Prepared Solution with Entity and Identity Frameworks using DB first approach

--- MathAPI/Models/IdentityModels.cs
+++ MathAPI/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace MathAPI.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("IdentityDbContext", throwIfV1Schema: false)
         {
         }
         
